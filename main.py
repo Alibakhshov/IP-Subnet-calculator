e73012ad58721fb54f1ae91f3fc1b12ed4b79f1a
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2022-07-28 13:06:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,9 @@ import sys
 app = QApplication(sys.argv)
 
 window = QWidget()
+
+# Window is hidden by default that is why we are shoeing it
 window.show()
+
+# starting up the event loop
 app.exec()
