b91ccf95fbc891c61f5413c2059b85a3b63e355e
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2022-07-28 13:59:34 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # importing from packages fromPyQt5
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QFrame
 import sys
 
 app = QApplication(sys.argv)
@@ -10,10 +10,16 @@ class MainWindow():
     win_width = 1500
     win_height = 900
     window.setGeometry(100, 100, win_width, win_height)
-    window.setStyleSheet(
-        "background-color: #696969;" 
+    # window.setStyleSheet(
+    #     "background-color: #696969;" 
 
-    )
+    # )
+    
+    
+    frame = QFrame(window)
+    frame.setFrameShape(QFrame.WinPanel)
+    frame.resize(500, 100)
+    frame.setFrameShadow(QFrame.Raised)
     # Window is hidden by default that is why we are showing it
     window.show()
     
