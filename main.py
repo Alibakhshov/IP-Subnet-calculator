4f7071d78f22c65e447d8836ab70e34fcc67ab4c
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2022-07-30 14:47:40 +0500

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ class MainWindow():
     main_frame.setFrameShape(QFrame.WinPanel)
     main_frame.setFrameShadow(QFrame.Raised)
     main_frame.setStyleSheet(
-        "border: 5px solid #708090;" +
+        "border: 5px solid #A9A9A9;" +
         "background-color: white;" +
         "border-radius: 25px;" 
         
